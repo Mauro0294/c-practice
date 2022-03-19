db44fedafb1289eb5b39aaa5bbd21262e57f7324
Mauro <mauro@Mauros-MBP.home>
2022-03-19 14:36:01 +0100

    Added newlines

--- pythagorean_theorem.c
+++ pythagorean_theorem.c
@@ -7,18 +7,18 @@
 int main()
 {
     int choice;
+    float side_a, side_b, side_c;
     printf("Welcome to the pythagorean theorem calculator!\n");
     printf("Which one applies to you?\n");
-    printf("1 - I have two short sides and I need to get the value of the long side\n");
-    printf("2 - I have the long side, one short side and I need to get the value of the other short side\n");
+    printf("\n1 - I have two short sides and I need to get the value of the long side\n");
+    printf("2 - I have the long side, one short side and I need to get the value of the other short side\n\n");
     scanf("%i", &choice);
     if (choice == 1) {
-        int side_a, side_b, side_c;
         // Input
         printf("Value of short side A: ");
-        scanf("%d", &side_a);
+        scanf("%f", &side_a);
         printf("Value of short side B: ");
-        scanf("%d", &side_b);
+        scanf("%f", &side_b);
 
         // Calculation
         side_c = (side_a * side_a) + (side_b * side_b);
@@ -26,7 +26,6 @@ int main()
         // Output
         printf("Value of the long side: %.2f", sqrt(side_c));
     } else if (choice == 2) {
-        float side_a, side_b, side_c;
         // Input
         printf("Value of the short side: ");
         scanf("%f", &side_a);
